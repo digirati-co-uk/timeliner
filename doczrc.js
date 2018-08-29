a53481b397a85ff04265b947ef25160265e4efb7
Stephen Fraser <stephen.fraser@digirati.com>
2018-08-29 16:06:59 +0100

    Debugging

--- doczrc.js
+++ doczrc.js
@@ -15,30 +15,4 @@ export default {
       preprocessor: 'sass',
     }),
   ],
-  modifyBundlerConfig: (config, ...args) => {
-    config.resolve.modules = [
-      'node_modules',
-      config.resolve.modules[0],
-      config.resolve.modules[3],
-    ];
-    config.resolveLoader.modules = [
-      'node_modules',
-      config.resolveLoader.modules[0],
-      config.resolveLoader.modules[3],
-    ];
-
-    config.module.rules = config.module.rules.map(rule => {
-      rule.exclude = [/node_modules/];
-      return rule;
-    });
-
-    config.plugins[0].config.threads = 1;
-    config.plugins[1].config.threads = 1;
-    config.watchOptions = {
-      poll: 100000,
-      ignored: /node_modules/,
-    };
-
-    return config;
-  },
 };
