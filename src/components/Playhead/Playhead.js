e451dfcf3ca3cdeb0a4e8582a0fbb9dadc71e4b3
Stephen Fraser <stephen.fraser@digirati.com>
2018-09-03 11:20:36 +0100

    Added playhead component stub

---
+++ src/components/Playhead/Playhead.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Playhead.scss';
+
+class Playhead extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Playhead;
