511d1697a52a21b712c1edb46bc73a2118b43129
Stephen Fraser <stephen.fraser@digirati.com>
2018-09-03 11:24:12 +0100

    Added timeline marker component stub

---
+++ src/components/TimelineMarker/TimelineMarker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './TimelineMarker.scss';
+
+class TimelineMarker extends Component {
+  static propTypes = {};
+
+  render() {
+    return <div />;
+  }
+}
+
+export default TimelineMarker;
