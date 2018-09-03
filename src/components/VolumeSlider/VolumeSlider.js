1a978ce1c8c72afeaf66ef5570edfb2a3ea9ad33
Stephen Fraser <stephen.fraser@digirati.com>
2018-09-03 12:13:07 +0100

    Small consistency changes

--- src/components/VolumeSlider/VolumeSlider.js
+++ src/components/VolumeSlider/VolumeSlider.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './VolumeSlider.scss';
 
-export class VolumeSlider extends Component {
+class VolumeSlider extends Component {
   static propTypes = {
+    /** Current volume value */
     volume: PropTypes.number.isRequired,
+    /** Handler for when volume is changed */
     onVolumeChanged: PropTypes.func.isRequired,
   };
   render() {
     return <div>VolumeSlider</div>;
   }
 }
+
+export default VolumeSlider;
