c4eaa6b75803c3f8422ff1c9591782d644eb015f
Adam Meszaros <adam.meszaros@digirati.com>
2018-09-04 12:08:31 +0100

    SVG Based Bubble Display Component

--- src/components/BubbleDisplay/BubbleDisplay.js
+++ src/components/BubbleDisplay/BubbleDisplay.js
@@ -5,7 +5,7 @@ import './BubbleDisplay.scss';
 class BubbleDisplay extends Component {
   static propTypes = {
     /** Map of points @todo custom validator */
-    points: PropTypes.object.isRequired,
+    points: PropTypes.array.isRequired,
     /** Width to display bubble UI */
     width: PropTypes.number.isRequired,
     /** Height to display bubble UI */
@@ -23,7 +23,55 @@ class BubbleDisplay extends Component {
   };
 
   render() {
-    return <div />;
+    const { width, height, zoom, x, points, children } = this.props;
+    const realWidth = width * zoom;
+    const maxWidth = Math.max.apply(null, points.map(point => point.to));
+    const maxDepth = 3; //TODO: compute max depth
+    const projectionFactor = realWidth / maxWidth;
+    const viewBox = [x * projectionFactor, 0, width, height].join(' ');
+    const bubbles = points.map(point => ({
+      x: point.from * projectionFactor,
+      width: (point.to - point.from) * projectionFactor,
+      colour: point.colour,
+      height: Math.pow(2 / 3, point.depth - 1) * height,
+      label: point.label,
+      point: point,
+    }));
+
+    const childrenWithProps =
+      typeof children === 'function'
+        ? children(bubbles)
+        : React.Children.map(children, child =>
+            React.cloneElement(child, { points: bubbles })
+          );
+
+    return (
+      <svg
+        width={width}
+        height={height}
+        viewBox={viewBox}
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+      >
+        <g
+          style={{
+            transform: 'scale(1, -1)',
+            transformOrigin: '50% 50%',
+          }}
+        >
+          <rect
+            width={width * zoom}
+            height={height}
+            style={{
+              fill: '#f6f6f6',
+              strokeWidth: 0,
+              stroke: 'rgb(0,0,0,0)',
+            }}
+          />
+          {childrenWithProps}
+        </g>
+      </svg>
+    );
   }
 }
 
--- src/components/SingleBubble/SingleBubble.js
+++ src/components/SingleBubble/SingleBubble.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import './SingleBubble.scss';
 
 class SingleBubble extends Component {
+  // NOTE: Changed the specification, the component doesn't really requires the y co-ordinate
   static propTypes = {
     /** Width of the bubble */
     width: PropTypes.number.isRequired,
@@ -10,14 +11,14 @@ class SingleBubble extends Component {
     height: PropTypes.number.isRequired,
     /** Position on X co-ordinate */
     x: PropTypes.number,
-    /** Position on Y co-ordinate */
-    y: PropTypes.number,
     /** Background colour of the bubble */
     colour: PropTypes.string,
     /** Active colour */
     activeColour: PropTypes.string,
     /** Click handler for the bubble */
     onClick: PropTypes.func,
+    /** Label for the bubble */
+    label: PropTypes.string,
   };
 
   static defaultProps = {
@@ -28,8 +29,35 @@ class SingleBubble extends Component {
     y: 0,
   };
 
+  onBubbleClick = ev => {
+    const { onClick } = this.props;
+    if (onClick) {
+      onClick(this.props.point, ev);
+    }
+  };
+
   render() {
-    return <svg />;
+    const { onClick, height, x, width, colour, label } = this.props;
+    const d = `M${x},0a${width / 2},${height} 0 0,0 ${width},0`;
+    return (
+      <g
+        onClick={this.onBubbleClick}
+        style={{
+          cursor: onClick ? 'pointer' : 'none',
+        }}
+      >
+        <path d={d} fill={colour} />
+        <text
+          textAnchor="middle"
+          fill="white"
+          x={width / 2 + x}
+          y={0}
+          transform={`scale(1,-1) translate(0,${-(2 / 3) * height})`}
+        >
+          {label}
+        </text>
+      </g>
+    );
   }
 }
 
