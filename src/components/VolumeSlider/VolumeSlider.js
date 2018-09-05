9bfc032c8ae40bdbb9a9b0bc292d7dd8f43311d2
Adam <33420570+adam-digirati@users.noreply.github.com>
2018-09-05 10:48:49 +0100

    Added volume slider and examples.  (#57)
    * Added volume slider and examples. closes #48
    
    * Added reach for the demos states and fixed #discussion_r214998389  #discussion_r214998259
    
    * https://github.com/digirati-co-uk/timeliner/pull/57#discussion_r215035282

--- src/components/VolumeSlider/VolumeSlider.js
+++ src/components/VolumeSlider/VolumeSlider.js
@@ -9,8 +9,79 @@ class VolumeSlider extends Component {
     /** Handler for when volume is changed */
     onVolumeChanged: PropTypes.func.isRequired,
   };
+  static defaultProps = {
+    volume: 100,
+  };
+  static instances = 0;
+
+  constructor(props) {
+    super(props);
+    this.instanceId = VolumeSlider.instances++;
+  }
+
+  onVolumeInputChange = ev => {
+    const { onVolumeChanged } = this.props;
+    if (onVolumeChanged) {
+      onVolumeChanged(parseInt(ev.target.value, 10));
+    }
+  };
+
   render() {
-    return <div>VolumeSlider</div>;
+    const { volume } = this.props;
+    const width = 294;
+    const maxVolume = 100;
+    const currentVolumeWidth = (volume / maxVolume) * width;
+    return (
+      <div className="volume-slider">
+        <svg
+          version="1.0"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0"
+          y="0"
+          viewBox={`0 0 ${width} 24`}
+          style={{
+            enableBackground: `new 0 0 ${width} 24`,
+          }}
+          xmlSpace="preserve"
+        >
+          <polygon
+            className="volume-slider__background"
+            points={`0,24 ${width},0 ${width},24`}
+          />
+          <g>
+            <defs>
+              <rect
+                id={`CurrentVolumeMask_${this.instanceId}`}
+                width={currentVolumeWidth}
+                height="24"
+              />
+            </defs>
+            <clipPath id={`CurrentVolumeMask_${this.instanceId}_`}>
+              <use
+                xlinkHref={`#CurrentVolumeMask_${this.instanceId}`}
+                style={{
+                  overflow: 'visible',
+                }}
+              />
+            </clipPath>
+            <polygon
+              className="volume-slider__foreground"
+              points={`0,24 ${width},0 ${width},24`}
+              clipPath={`url("#CurrentVolumeMask_${this.instanceId}_")`}
+            />
+          </g>
+        </svg>
+        <input
+          type="range"
+          className="volume-slider__range"
+          min="0"
+          max={maxVolume}
+          value={volume}
+          onChange={this.onVolumeInputChange}
+        />
+      </div>
+    );
   }
 }
 
