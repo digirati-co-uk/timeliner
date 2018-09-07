5992f0909b7980e03a12cb989a30399f6e860444
Adam Meszaros <adam.meszaros@digirati.com>
2018-09-07 13:18:47 +0100

    Metadata display

--- src/components/MetadataDisplay/MetadataDisplay.js
+++ src/components/MetadataDisplay/MetadataDisplay.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './MetadataDisplay.scss';
+import bem from '@fesk/bem-js';
+import formatDate from 'date-fns/format';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  CardActions,
+  IconButton,
+} from '@material-ui/core';
+import { Edit } from '@material-ui/icons';
 
-class MetadataDisplay extends Component {
-  static propTypes = {
-    /** Label of the manifest or range */
-    label: PropTypes.string.isRequired,
-    /** Summary of the manifest or range */
-    summary: PropTypes.string.isRequired,
-    /** Time when metadata should be displayed from */
-    startTime: PropTypes.number.isRequired,
-    /** Time when metadata should be displayed until */
-    endTime: PropTypes.number.isRequired,
-  };
+const displayTime = time =>
+  formatDate(time, time >= 3600000 ? 'hh:mm:ss' : 'mm:ss');
 
-  render() {
-    return <div />;
-  }
-}
+const MetadataDisplay = props => (
+  <Card>
+    <CardHeader
+      title={`${props.label} [from ${displayTime(
+        props.startTime
+      )} to ${displayTime(props.endTime)}`}
+      action={
+        <IconButton>
+          <Edit />
+        </IconButton>
+      }
+    />
+    <CardContent>
+      <Typography variant="body1" component="pre">
+        {props.summary}
+      </Typography>
+    </CardContent>
+  </Card>
+);
+
+MetadataDisplay.propTypes = {
+  /** Label of the manifest or range */
+  label: PropTypes.string.isRequired,
+  /** Summary of the manifest or range */
+  summary: PropTypes.string.isRequired,
+  /** Time when metadata should be displayed from */
+  startTime: PropTypes.number.isRequired,
+  /** Time when metadata should be displayed until */
+  endTime: PropTypes.number.isRequired,
+};
 
 export default MetadataDisplay;
