7931203e3e046fb58268d2b268beb5a627e7b057
Adam <33420570+adam-digirati@users.noreply.github.com>
2018-09-10 14:00:04 +0100

    added keyboard controls provider (#60)
    * added keyboard controls provider
    
    * fixed keyboard combinations

---
+++ src/components/KeyboardControlsProvider/KeyboardControlsProvider.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
+
+const emptyFn = () => {};
+const MODIFIER_KEYS = ['Meta', 'Shift', 'Control', 'Alt'];
+
+const getKeyCombinationPressed = (key, ev) => {
+  let keyCombo = [];
+  if (key === 'other') {
+    if (ev.metaKey) {
+      keyCombo.push('meta');
+    }
+    if (ev.ctrlKey) {
+      keyCombo.push('ctrl');
+    }
+    if (ev.altKey) {
+      keyCombo.push('alt');
+    }
+    if (ev.shiftKey) {
+      keyCombo.push('shift');
+    }
+    if (MODIFIER_KEYS.indexOf(ev.key) === -1) {
+      keyCombo.push(String.fromCharCode(ev.keyCode));
+    }
+  } else {
+    keyCombo = [key];
+  }
+  return keyCombo.join('+').toLowerCase();
+};
+
+const KeyboardControlsProvider = props => (
+  <KeyboardEventHandler
+    handleKeys={Object.keys(props.commands)}
+    onKeyEvent={(key, ev) => {
+      const keyCombo = getKeyCombinationPressed(key, ev);
+      (props.commands[keyCombo] || props.commands.all || emptyFn)(
+        keyCombo,
+        key,
+        ev
+      );
+    }}
+  />
+);
+
+KeyboardControlsProvider.propTypes = {
+  commands: PropTypes.object.isRequired,
+};
+
+KeyboardControlsProvider.defaultProps = {
+  commands: {},
+};
+
+export default KeyboardControlsProvider;
