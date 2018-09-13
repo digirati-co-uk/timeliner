2f06c1f3cd229470ec5d26ece4bf930f86da91a7
Adam <33420570+adam-digirati@users.noreply.github.com>
2018-09-13 17:06:57 +0100

    Added state management docs (#68)
    * Added state management docs
    
    * Added quick table component

---
+++ src/components/docs/components/Table/Table.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import BEM from '@fesk/bem-js';
+import './Table.scss';
+
+const $b = BEM.block('table');
+const Table = ({ children }) => <table className={$b}>{children}</table>;
+Table.Header = ({ children }) => (
+  <thead className={$b.element('header')}>{children}</thead>
+);
+Table.Row = ({ children }) => <tr className={$b.element('row')}>{children}</tr>;
+Table.Cell = ({ children, ...props }) => (
+  <td className={$b.element('cell')} {...props}>
+    {children}
+  </td>
+);
+
+export default Table;
