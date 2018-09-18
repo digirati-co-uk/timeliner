b82c93656a1e09bdd8b2cee49d3526ad2005b1f0
Adam Meszaros <adam.meszaros@digirati.com>
2018-09-18 07:52:21 +0100

    bubble display as well

--- src/components/BubbleDisplay/BubbleDisplay.js
+++ src/components/BubbleDisplay/BubbleDisplay.js
@@ -65,14 +65,7 @@ class BubbleDisplay extends Component {
         width={width}
         height={height}
         viewBox={viewBox}
-<<<<<<< HEAD
-        style={{
-          verticalAlign: 'top',
-          display: 'block',
-        }}
-=======
         className="bubble-display"
->>>>>>> master
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
       >
