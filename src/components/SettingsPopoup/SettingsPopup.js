ad94795aa9a8cd1549f39ca40a2199b0c66f5475
Adam Meszaros <adam.meszaros@digirati.com>
2018-09-20 14:34:46 +0100

    fixed column warnings on grid containers

--- src/components/SettingsPopoup/SettingsPopup.js
+++ src/components/SettingsPopoup/SettingsPopup.js
@@ -77,180 +77,181 @@ export default class SettingsPopup extends React.Component {
               container
               direction="row"
               justify="stretch"
-              sm={12}
               alignItems="stretch"
               spacing={16}
               style={{
                 width: 700,
               }}
             >
-              <Grid
-                container
-                direction="column"
-                justify="flex-start"
-                md={6}
-                sm={12}
-                spacing={16}
-              >
-                {/* 
-                {"These properties are not needed on the web version."}
-                <Grid item>
-                  <FormControl component="fieldset">
-                    <FormLabel component="legend">Timeline is...</FormLabel>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={this.state.editable}
-                            onChange={this.handleChange('editable', 'checkbox')}
-                            value="editable"
-                          />
-                        }
-                        label="Editable"
-                      />
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={this.state.resizable}
-                            onChange={this.handleChange('resizable', 'checkbox')}
-                            value="resizable"
-                          />
-                        }
-                        label="Resizable"
-                      />
-                    </FormGroup>
-                  </FormControl>
-                </Grid> */}
-                <Grid item>
-                  <FormControl component="fieldset">
-                    <FormLabel component="legend">
-                      Timeline Appearance
-                    </FormLabel>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={this.state.showTimes}
-                            onChange={this.handleChange(
-                              'showTimes',
-                              'checkbox'
-                            )}
-                            value="showTimes"
-                          />
-                        }
-                        label="Show Times"
-                      />
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={this.state.blackAndWhite}
-                            onChange={this.handleChange(
-                              'blackAndWhite',
-                              'checkbox'
-                            )}
-                            value="blackAndWhite"
-                          />
-                        }
-                        label="Black and White"
-                      />
-                      <FormControl component="fieldset">
-                        <FormLabel component="legend">Bubble Shape</FormLabel>
-                        <RadioGroup
-                          aria-label="bubble shape"
-                          name="bubbleShape"
-                          value={this.state.bubbleShape}
-                          onChange={this.handleChange('bubbleShape')}
-                        >
-                          <FormControlLabel
-                            value="round"
-                            control={<Radio color="primary" />}
-                            label="Round"
-                            labelPlacement="end"
-                          />
-                          <FormControlLabel
-                            value="square"
-                            control={<Radio color="primary" />}
-                            label="Square"
-                            labelPlacement="end"
-                          />
-                        </RadioGroup>
-                      </FormControl>
-                    </FormGroup>
-                  </FormControl>
+              <Grid item md={6} sm={12}>
+                <Grid
+                  container
+                  direction="column"
+                  justify="flex-start"
+                  spacing={16}
+                >
+                  {/* 
+                  {"These properties are not needed on the web version."}
+                  <Grid item>
+                    <FormControl component="fieldset">
+                      <FormLabel component="legend">Timeline is...</FormLabel>
+                      <FormGroup>
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={this.state.editable}
+                              onChange={this.handleChange('editable', 'checkbox')}
+                              value="editable"
+                            />
+                          }
+                          label="Editable"
+                        />
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={this.state.resizable}
+                              onChange={this.handleChange('resizable', 'checkbox')}
+                              value="resizable"
+                            />
+                          }
+                          label="Resizable"
+                        />
+                      </FormGroup>
+                    </FormControl>
+                  </Grid> */}
+                  <Grid item>
+                    <FormControl component="fieldset">
+                      <FormLabel component="legend">
+                        Timeline Appearance
+                      </FormLabel>
+                      <FormGroup>
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={this.state.showTimes}
+                              onChange={this.handleChange(
+                                'showTimes',
+                                'checkbox'
+                              )}
+                              value="showTimes"
+                            />
+                          }
+                          label="Show Times"
+                        />
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={this.state.blackAndWhite}
+                              onChange={this.handleChange(
+                                'blackAndWhite',
+                                'checkbox'
+                              )}
+                              value="blackAndWhite"
+                            />
+                          }
+                          label="Black and White"
+                        />
+                        <FormControl component="fieldset">
+                          <FormLabel component="legend">Bubble Shape</FormLabel>
+                          <RadioGroup
+                            aria-label="bubble shape"
+                            name="bubbleShape"
+                            value={this.state.bubbleShape}
+                            onChange={this.handleChange('bubbleShape')}
+                          >
+                            <FormControlLabel
+                              value="round"
+                              control={<Radio color="primary" />}
+                              label="Round"
+                              labelPlacement="end"
+                            />
+                            <FormControlLabel
+                              value="square"
+                              control={<Radio color="primary" />}
+                              label="Square"
+                              labelPlacement="end"
+                            />
+                          </RadioGroup>
+                        </FormControl>
+                      </FormGroup>
+                    </FormControl>
+                  </Grid>
                 </Grid>
               </Grid>
-              <Grid
-                container
-                direction="column"
-                justify="flex-start"
-                md={6}
-                sm={12}
-                spacing={16}
-              >
-                <Grid item>
-                  <FormControl component="fieldset">
-                    <FormLabel component="legend">Bubble Height</FormLabel>
-                    <FormGroup>
-                      <Slider
-                        onChange={this.handleSliderChange}
-                        value={this.state.bubbleHeight}
-                      />
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={this.state.autoScaleHeightOnResize}
-                            onChange={this.handleChange(
-                              'autoScaleHeightOnResize',
-                              'checkbox'
-                            )}
-                            value="autoScaleHeightOnResize"
-                          />
-                        }
-                        label="Auto Scale Height On Resize"
-                      />
-                    </FormGroup>
-                  </FormControl>
-                </Grid>
-                <Grid item>
-                  <FormControl component="fieldset">
-                    <FormLabel component="legend">
-                      Bubble Level Colours
-                    </FormLabel>
-                    <FormGroup>TODO: Colour theme editor</FormGroup>
-                  </FormControl>
-                </Grid>
-                <Grid item>
-                  <FormControl component="fieldset">
-                    <FormLabel component="legend">Audio Settings</FormLabel>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={this.state.startPlayingWhenBubbleClicked}
-                            onChange={this.handleChange(
-                              'startPlayingWhenBubbleClicked',
-                              'checkbox'
-                            )}
-                            value="startPlayingWhenBubbleClicked"
-                          />
-                        }
-                        label="Start playing when bubble is clicked."
-                      />
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={this.state.stopPlayingAtTheEndOfTheSection}
-                            onChange={this.handleChange(
-                              'stopPlayingAtTheEndOfTheSection',
-                              'checkbox'
-                            )}
-                            value="stopPlayingAtTheEndOfTheSection"
-                          />
-                        }
-                        label="Stop Playing at the end of the section."
-                      />
-                    </FormGroup>
-                  </FormControl>
+              <Grid item md={6} sm={12}>
+                <Grid
+                  container
+                  direction="column"
+                  justify="flex-start"
+                  spacing={16}
+                >
+                  <Grid item>
+                    <FormControl component="fieldset">
+                      <FormLabel component="legend">Bubble Height</FormLabel>
+                      <FormGroup>
+                        <Slider
+                          onChange={this.handleSliderChange}
+                          value={this.state.bubbleHeight}
+                        />
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={this.state.autoScaleHeightOnResize}
+                              onChange={this.handleChange(
+                                'autoScaleHeightOnResize',
+                                'checkbox'
+                              )}
+                              value="autoScaleHeightOnResize"
+                            />
+                          }
+                          label="Auto Scale Height On Resize"
+                        />
+                      </FormGroup>
+                    </FormControl>
+                  </Grid>
+                  <Grid item>
+                    <FormControl component="fieldset">
+                      <FormLabel component="legend">
+                        Bubble Level Colours
+                      </FormLabel>
+                      <FormGroup>TODO: Colour theme editor</FormGroup>
+                    </FormControl>
+                  </Grid>
+                  <Grid item>
+                    <FormControl component="fieldset">
+                      <FormLabel component="legend">Audio Settings</FormLabel>
+                      <FormGroup>
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={this.state.startPlayingWhenBubbleClicked}
+                              onChange={this.handleChange(
+                                'startPlayingWhenBubbleClicked',
+                                'checkbox'
+                              )}
+                              value="startPlayingWhenBubbleClicked"
+                            />
+                          }
+                          label="Start playing when bubble is clicked."
+                        />
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={
+                                this.state.stopPlayingAtTheEndOfTheSection
+                              }
+                              onChange={this.handleChange(
+                                'stopPlayingAtTheEndOfTheSection',
+                                'checkbox'
+                              )}
+                              value="stopPlayingAtTheEndOfTheSection"
+                            />
+                          }
+                          label="Stop Playing at the end of the section."
+                        />
+                      </FormGroup>
+                    </FormControl>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
