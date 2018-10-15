8618f09d96ebb2c1927ecc6dbc47c4762b638b08
Adam <33420570+adam-digirati@users.noreply.github.com>
2018-10-15 14:24:07 +0100

    fix the firefox export document issue (#162)
    * fix the firefox export document issue
    
    * cleanup

--- src/utils/fileDownload.js
+++ src/utils/fileDownload.js
@@ -1,4 +1,5 @@
 export const immediateDownload = (dataStr, mime_type = 'application/json') => {
+  const body = document.body;
   const blob = new Blob([dataStr], {
     type: mime_type,
   });
@@ -13,7 +14,8 @@ export const immediateDownload = (dataStr, mime_type = 'application/json') => {
       window.URL.revokeObjectURL(that.href);
     }, 1500);
   };
-
+  body.appendChild(dlink);
   dlink.click();
   dlink.remove();
+  body.removeChild(dlink);
 };
