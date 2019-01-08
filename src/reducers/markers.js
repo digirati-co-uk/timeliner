97a62882221155e91b71c82dc52a3489e1fe46a4
stephenwf <stephen.fraser@digirati.com>
2019-01-08 10:48:12 +0000

    Markers state

---
+++ src/reducers/markers.js
@@ -0,0 +1,78 @@
+import update from 'immutability-helper';
+import {
+  UPDATE_MARKER,
+  IMPORT_MARKERS,
+  DELETE_MARKER,
+  SHOW_MARKERS,
+  HIDE_MARKERS,
+  MARKER,
+  SELECT_MARKER,
+  DESELECT_MARKER,
+} from '../constants/markers';
+
+export const DEFAULT_STATE = {
+  list: {},
+  selected: [],
+  visible: true,
+};
+
+function filterUndefinedSets(acc, [key, change]) {
+  if (typeof change.$set !== 'undefined') {
+    acc[key] = change;
+  }
+  return acc;
+}
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case UPDATE_MARKER:
+      if (!action.payload.id) {
+        return state;
+      }
+      return update(state, {
+        list: {
+          [action.payload.id]: [
+            [MARKER.ID, { $set: action.payload.id }],
+            [MARKER.LABEL, { $set: action.payload.label }],
+            [MARKER.SUMMARY, { $set: action.payload.summary }],
+            [MARKER.TIME, { $set: action.payload.time }],
+          ].reduce(filterUndefinedSets, {}),
+        },
+      });
+    case IMPORT_MARKERS:
+      return update(state, {
+        list: action.payload.markers.reduce((acc, next) => {
+          if (next.id) {
+            acc[next.id] = { $set: next };
+          }
+          return acc;
+        }, {}),
+      });
+    case DELETE_MARKER:
+      return update(state, {
+        list: { $unset: [action.payload.id] },
+      });
+    case SHOW_MARKERS:
+      return update(state, {
+        visible: { $set: true },
+      });
+    case HIDE_MARKERS:
+      return update(state, {
+        visible: { $set: false },
+      });
+    case SELECT_MARKER:
+      return update(state, {
+        selected: { $push: [action.payload.id] },
+      });
+    case DESELECT_MARKER:
+      const index = state.selected.indexOf(action.payload.id);
+      if (index === -1) {
+        return state;
+      }
+      return update(state, {
+        selected: { $splice: [[state.selected.indexOf(action.payload.id), 1]] },
+      });
+    default:
+      return state;
+  }
+}
