ac8b069197b76ae3825c4ce7bb3675539af68fc8
Stephen Fraser <stephenwf@users.noreply.github.com>
2019-02-14 12:19:13 +0000

    Changed how saving works (#261)

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import Root from './containers/Root/Root';
 import configureStore from './store/main';
 import * as qs from 'query-string';
 
-const { resource, fresh, ...hash } = qs.parse(location.hash);
+const { resource, save, ...hash } = qs.parse(location.hash);
 
 // Set the hash back
-location.hash = qs.stringify({ resource, ...hash });
+location.hash = qs.stringify({ resource, save, ...hash });
 
-const { store, persistor } = configureStore(resource, fresh);
+const { store, persistor } = configureStore(
+  resource,
+  typeof save === 'undefined'
+);
 
 render(
   <Root store={store} persistor={persistor} />,
