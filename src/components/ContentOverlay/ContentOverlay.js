d1a0fd2fbd1857160810af3654a7e001da753ef3
Stephen Fraser <stephen.fraser@digirati.com>
2019-03-07 00:52:49 +0000

    Added "continue anyway" to audio error page

--- src/components/ContentOverlay/ContentOverlay.js
+++ src/components/ContentOverlay/ContentOverlay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import LoadingIndicator from '../LoadingIndicator/LoadingIndicator';
@@ -11,47 +11,60 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/es/DialogActions/DialogActions';
 import Button from '@material-ui/core/es/Button/Button';
 
-const ContentOverlay = props => (
-  <div className="content-overlay">
-    {props.error.code ? (
-      <Dialog open={true} aria-labelledby="form-dialog-title">
-        <DialogTitle>We are unable to play this content</DialogTitle>
-        <DialogContent>
-          <DialogContentText style={{ color: 'red', marginBottom: 20 }}>
-            {props.error.description}
-          </DialogContentText>
-          <DialogContentText>
-            <a href={props.audioUrl} target="_blank">
-              {props.audioUrl}
-            </a>
-          </DialogContentText>
-          <DialogContentText>
-            Please refresh the page to try another resource.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={() => window.location.reload(true)}
-          >
-            Refresh page
-          </Button>
-        </DialogActions>
-      </Dialog>
-    ) : !props.isLoaded ? (
-      <LoadingIndicator loadingPercent={props.loadingPercent} />
-    ) : (
-      ''
-    )}
-  </div>
-);
+const ContentOverlay = props => {
+  const [dismissed, setDismissed] = useState(false);
 
+  if (dismissed) return null;
+
+  return (
+    <div className="content-overlay">
+      {props.error.code ? (
+        <Dialog open={true} aria-labelledby="form-dialog-title">
+          <DialogTitle>We are unable to play this content</DialogTitle>
+          <DialogContent>
+            <DialogContentText style={{ color: 'red', marginBottom: 20 }}>
+              {props.error.description}
+            </DialogContentText>
+            <DialogContentText>
+              <a href={props.audioUrl} target="_blank">
+                {props.audioUrl}
+              </a>
+            </DialogContentText>
+            <DialogContentText>
+              Please refresh the page to try another resource.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              color="primary"
+              variant="outlined"
+              onClick={() => setDismissed(true)}
+            >
+              Continue anyway
+            </Button>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={() => window.location.reload(true)}
+            >
+              Refresh page
+            </Button>
+          </DialogActions>
+        </Dialog>
+      ) : !props.isLoaded ? (
+        <LoadingIndicator loadingPercent={props.loadingPercent} />
+      ) : (
+        ''
+      )}
+    </div>
+  );
+};
 ContentOverlay.propTypes = {
   loadingPercent: PropTypes.number.isRequired,
   error: PropTypes.object,
   audioUrl: PropTypes.string,
   isLoaded: PropTypes.bool,
+  onContinue: PropTypes.func,
 };
 
 export default ContentOverlay;
