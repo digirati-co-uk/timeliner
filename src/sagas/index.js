3eb59677081e91f44bcbdc9a79fbc982d7d13268
Brian Keese <bkeese@indiana.edu>
2019-04-01 15:49:23 -0400

    Update index.js
    Fix typo

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -294,7 +294,7 @@ function saveResource(url, content) {
         if (200 <= http.status && http.status <= 299) {
           // reload parent widow to location of newly created timeline
           if (document.referrer!=http.getResponseHeader('location')){
-            reject({rediirect_location: http.getResponseHeader('location')})
+            reject({redirect_location: http.getResponseHeader('location')})
             return;
           }
           resolve();
