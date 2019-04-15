1c6610e4db7ef7044d4f67301a96f2cf480affbf
Chris Colvard <cjcolvar@indiana.edu>
2019-04-15 15:15:27 -0400

    Clean up language

--- src/constants/canvas.js
+++ src/constants/canvas.js
@@ -24,11 +24,8 @@ export const SET_CURRENT_TIME = 'SET_CURRENT_TIME';
 export const LOAD_CANVAS = 'LOAD_CANVAS';
 export const UNLOAD_AUDIO = 'UNLOAD_AUDIO';
 export const ERROR_CODES = {
-  MEDIA_ERR_ABORTED: "Client aborted download at user's request",
-  MEDIA_ERR_NETWORK:
-    'A network error of some description caused the user agent to stop fetching the media resource, after the resource was established to be usable.',
-  MEDIA_ERR_DECODE:
-    'An error of some description occurred while decoding the media resource, after the resource was established to be usable.',
-  MEDIA_ERR_SRC_NOT_SUPPORTED:
-    'Media (audio file) not supported ("not usable.")',
+  MEDIA_ERR_ABORTED: 'Download aborted',
+  MEDIA_ERR_NETWORK: 'A network error occurred',
+  MEDIA_ERR_DECODE: 'An error occurred while decoding the media resource',
+  MEDIA_ERR_SRC_NOT_SUPPORTED: 'Media file format not supported',
 };
